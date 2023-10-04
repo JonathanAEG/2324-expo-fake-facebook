dfc3c66d9f9587f9f61e3c1d4c02c5d3ad267030
Jonathan Ramos <jonathan.ramos@ikasle.aeg.eus>
2023-10-04 11:55:44 +0200

    [ADD] Importados los componentes de Screens al fichero App.js

--- App.js
+++ App.js
@@ -3,7 +3,12 @@ import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
 import React from 'react';
 import 'react-native-gesture-handler';
 import Header from './components/Header';
-
+import Home from './components/Screens/Home';
+import Friends from './components/Screens/Friends';
+import Groups from './components/Screens/Groups';
+import Profile from './components/Screens/Profile';
+import Notifications from './components/Screens/Notifications';
+import Menu from './components/Screens/Menu';
 
 export default function App() {
   return (
