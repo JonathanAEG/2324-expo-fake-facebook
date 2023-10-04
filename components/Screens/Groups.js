56b62e0824ee3df4a25339ce68aba5a9a007179c
Jonathan Ramos <jonathan.ramos@ikasle.aeg.eus>
2023-10-04 11:51:47 +0200

    [NEW] Creados ficheros para la carpeta de Screens

---
+++ components/Screens/Groups.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components";
+
+const View = styled.View`
+
+    align-items: center;
+    height: 500px;
+`
+export default Groups=()=>{
+
+    return(
+
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
